14bcfc2a551aa9aa1ca356fd8865e30d50496775
Milos Foltan <foltan.milosml@gmail.com>
2021-03-21 10:49:34 +0100

    all  moving

---
+++ src/app/pages/services/services.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.scss']
+})
+export class ServicesComponent implements OnInit {
+
+  public array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
